4cbee171b0554db66d9802807fc4759e3040626b
Finnegan1 <fynn.borrmeister@gmail.com>
2024-04-16 15:14:20 +0200

    .

--- app/veranstaltungen/[event]/page.tsx
+++ app/veranstaltungen/[event]/page.tsx
@@ -3,7 +3,7 @@
 import { Event } from "@/components/event"
 import { pocketBase } from "@/lib/pocketBase"
 
-export async function getStaticParams() {
+export async function generateStaticParams() {
     const events = await pocketBase.collection("events").getFullList();
     return events.map((event) => ({
         params: { event: event.nameShort.replaceAll(" ", "_") },

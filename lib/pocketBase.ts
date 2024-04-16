44125f84ba4871708dc94c7c7c26f1b2c31b761f
Finnegan1 <fynn.borrmeister@gmail.com>
2024-04-16 15:39:50 +0200

    .

--- lib/pocketBase.ts
+++ lib/pocketBase.ts
@@ -2,3 +2,4 @@ import PocketBase from "pocketbase";
 import { TypedPocketBase } from "./pocketbase-types";
 
 export const pocketBase: TypedPocketBase = new PocketBase("https://invoice-pioneer.pockethost.io");
+pocketBase.autoCancellation(false);
